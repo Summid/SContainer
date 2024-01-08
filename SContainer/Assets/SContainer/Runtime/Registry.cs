b57324f5b90826b3c3024bbd25645b48e5b49229
Summid <summid@foxmail.com>
2024-01-08 23:26:13 +0800

    update Registry; add SContainerException

--- SContainer/Assets/SContainer/Runtime/Registry.cs
+++ SContainer/Assets/SContainer/Runtime/Registry.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SContainer.Runtime.Internal;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SContainer.Runtime
 {
@@ -8,11 +10,49 @@ namespace SContainer.Runtime
     /// </summary>
     public sealed class Registry
     {
+        [ThreadStatic]  // this field is unique to every thread
         private static IDictionary<Type, Registration> buildBuffer = new Dictionary<Type, Registration>(128);
 
+        private FixedTypeKeyHashTable<Registration> hashTable;
+        
         public static Registry Build(Registration[] registrations)
         {
-            throw new NotImplementedException();
+            if (buildBuffer == null)
+                buildBuffer = new Dictionary<Type, Registration>(128);
+            buildBuffer.Clear();
+            
+            foreach (var registration in registrations)
+            {
+                AddToBuildBuffer(buildBuffer, registration.ImplementationType, registration);
+            }
+
+            var hashTable = new FixedTypeKeyHashTable<Registration>(buildBuffer.ToArray());
+            return new Registry(hashTable);
+        }
+
+        private static void AddToBuildBuffer(IDictionary<Type, Registration> buf, Type service, Registration registration)
+        {
+            if (buf.TryGetValue(service, out var exists))
+            {
+                throw new SContainerException(service, $"Duplicate implementation type is not supported currently.");
+            }
+            else
+            {
+                buf.Add(service, registration);
+            }
+        }
+
+        private Registry(FixedTypeKeyHashTable<Registration> hashTable)
+        {
+            this.hashTable = hashTable;
+        }
+
+        public bool TryGet(Type interfaceType, out Registration registration)
+        {
+            if (this.hashTable.TryGet(interfaceType, out registration))
+                return registration != null;
+
+            return false;
         }
     }
 }
---
+++ SContainer/Assets/SContainer/Runtime/SContainerException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SContainer.Runtime
+{
+    public class SContainerException : Exception
+    {
+        public readonly Type InvalidType;
+
+        public SContainerException(Type invalidType, string message) : base(message)
+        {
+            this.InvalidType = invalidType;
+        }
+    }
+}
