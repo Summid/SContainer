f5b954e680f4e4751213d091a0e4f4493087caae
Summid <summid@foxmail.com>
2024-01-09 23:07:23 +0800

    add InjectAttribute

---
+++ SContainer/Assets/SContainer/Runtime/Annotations/InjectAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace SContainer.Runtime.Annotations
+{
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class InjectAttribute : Attribute { }
+}
