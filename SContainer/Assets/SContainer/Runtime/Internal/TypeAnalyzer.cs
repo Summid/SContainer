b43fc936f2f27e9ae279f1e6ac171b08a541cc28
Summid <summid@foxmail.com>
2024-01-10 19:02:22 +0800

    check circular dependency

--- SContainer/Assets/SContainer/Runtime/Internal/TypeAnalyzer.cs
+++ SContainer/Assets/SContainer/Runtime/Internal/TypeAnalyzer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace SContainer.Runtime.Internal
@@ -58,12 +59,83 @@ namespace SContainer.Runtime.Internal
         }
     }
 
+    internal readonly struct DependencyInfo
+    {
+        public Type ImplementationType => this.Dependency.ImplementationType;
+        public IInstanceProvider Provider => this.Dependency.Provider;
+        
+        public readonly Registration Dependency;
+        private readonly Registration owner;  // whose objects need to be checked 
+        private readonly object method;  // ctor or method or field or prop
+        private readonly ParameterInfo param;
+
+        public DependencyInfo(Registration dependency)
+        {
+            this.Dependency = dependency;
+            this.owner = null;
+            this.method = null;
+            this.param = null;
+        }
+
+        public DependencyInfo(Registration dependency, Registration owner, ConstructorInfo ctor, ParameterInfo param)
+        {
+            this.Dependency = dependency;
+            this.owner = owner;
+            this.method = ctor;
+            this.param = param;
+        }
+
+        public DependencyInfo(Registration dependency, Registration owner, MethodInfo method, ParameterInfo param)
+        {
+            this.Dependency = dependency;
+            this.owner = owner;
+            this.method = method;
+            this.param = param;
+        }
+        
+        public DependencyInfo(Registration dependency, Registration owner, FieldInfo field)
+        {
+            this.Dependency = dependency;
+            this.owner = owner;
+            this.method = field;
+            this.param = null;
+        }
+
+        public DependencyInfo(Registration dependency, Registration owner, PropertyInfo prop)
+        {
+            this.Dependency = dependency;
+            this.owner = owner;
+            this.method = prop;
+            this.param = null;
+        }
+        
+        public override string ToString()
+        {
+            switch (this.method)
+            {
+                case ConstructorInfo _:
+                    return $"{this.owner.ImplementationType}..ctor({this.param.Name})";
+                case MethodInfo methodInfo:
+                    return $"{this.owner.ImplementationType.FullName}.{methodInfo.Name}({this.param.Name})";
+                case FieldInfo field:
+                    return $"{this.owner.ImplementationType.FullName}.{field.Name}";
+                case PropertyInfo prop:
+                    return $"{this.owner.ImplementationType.FullName}.{prop.Name}";
+                default:
+                    return "";
+            }
+        }
+    }
+    
     internal static class TypeAnalyzer
     {
         public static InjectTypeInfo AnalyzeWithCode(Type type) => Cache.GetOrAdd(type, AnalyzeFunc); 
 
         private static readonly ConcurrentDictionary<Type, InjectTypeInfo> Cache = new ConcurrentDictionary<Type, InjectTypeInfo>();
 
+        [ThreadStatic]
+        private static Stack<DependencyInfo> circularDependencyChecker;
+        
         private static readonly Func<Type, InjectTypeInfo> AnalyzeFunc = Analyze;
         
         public static InjectTypeInfo Analyze(Type type)
@@ -201,6 +273,97 @@ namespace SContainer.Runtime.Internal
                 injectMethods,
                 injectFields,
                 injectProperties);
+        }
+
+        public static void CheckCircularDependency(IReadOnlyList<Registration> registrations, Registry registry)
+        {
+            // ThreadStatic
+            if (circularDependencyChecker == null)
+                circularDependencyChecker = new Stack<DependencyInfo>();
+
+            for (var i = 0; i < registrations.Count; i++)
+            {
+                circularDependencyChecker.Clear();
+                CheckCircularDependencyRecursive(new DependencyInfo(registrations[i]), registry, circularDependencyChecker);
+            }
+        }
+        
+        private static void CheckCircularDependencyRecursive(DependencyInfo current, Registry registry, Stack<DependencyInfo> stack)
+        {
+            // stack 中的对象，不能被其它对象依赖，如果有，则说明有循环依赖错误
+
+            var i = 0;
+            foreach (var dependency in stack)
+            {
+                if (current.ImplementationType == dependency.ImplementationType)
+                {
+                    stack.Push(current);
+                    
+                    var path = string.Join("\n",
+                        stack.Take(i + 1)
+                            .Reverse()
+                            .Select((item, itemIndex) => $"    [{itemIndex + 1}] {item} --> {item.ImplementationType.FullName}"));
+                    throw new SContainerException(current.Dependency.ImplementationType,
+                        $"Circular dependency detected!\n{path}");
+                }
+                i++;
+            }
+
+            stack.Push(current);
+
+            if (Cache.TryGetValue(current.ImplementationType, out var injectTypeInfo))
+            {
+                if (injectTypeInfo.InjectConstructorInfo != null)
+                {
+                    foreach (var x in injectTypeInfo.InjectConstructorInfo.ParameterInfos)
+                    {
+                        // 检查自身构造方法的参数，如果这个参数类型被注册，则检查循环依赖，即检查参数是否依赖自身
+                        if (registry.TryGet(x.ParameterType, out var parameterRegistration))
+                        {
+                            CheckCircularDependencyRecursive(new DependencyInfo(parameterRegistration, current.Dependency, injectTypeInfo.InjectConstructorInfo.ConstructorInfo, x), registry, stack);
+                        }
+                    }
+                }
+
+                if (injectTypeInfo.InjectMethodInfos != null)
+                {
+                    // 检查自身 [Inject]Method，和检查构造方法一样，检查其每一个参数
+                    foreach (var methodInfo in injectTypeInfo.InjectMethodInfos)
+                    {
+                        foreach (var x in methodInfo.ParameterInfos)
+                        {
+                            if (registry.TryGet(x.ParameterType, out var parameterRegistration))
+                            {
+                                CheckCircularDependencyRecursive(new DependencyInfo(parameterRegistration, current.Dependency, methodInfo.MethodInfo, x), registry, stack);
+                            }
+                        }
+                    }
+                }
+                
+                if (injectTypeInfo.InjectFields != null)
+                {
+                    foreach (var x in injectTypeInfo.InjectFields)
+                    {
+                        if (registry.TryGet(x.FieldType, out var fieldRegistration))
+                        {
+                            CheckCircularDependencyRecursive(new DependencyInfo(fieldRegistration, current.Dependency, x), registry, stack);
+                        }
+                    }
+                }
+                
+                if (injectTypeInfo.InjectProperties != null)
+                {
+                    foreach (var x in injectTypeInfo.InjectProperties)
+                    {
+                        if (registry.TryGet(x.PropertyType, out var propertyRegistration))
+                        {
+                            CheckCircularDependencyRecursive(new DependencyInfo(propertyRegistration, current.Dependency, x), registry, stack);
+                        }
+                    }
+                }
+            }
+
+            stack.Pop();
         }
     }
 }
