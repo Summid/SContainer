ac47b8b7fabbb51ab3dfa0bfaab6c7a47720ca18
Summid <summid@foxmail.com>
2024-01-11 16:12:15 +0800

    add CompositeDisposable

---
+++ SContainer/Assets/SContainer/Runtime/Internal/CompositeDisposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace SContainer.Runtime.Internal
+{
+    internal sealed class CompositeDisposable : IDisposable
+    {
+        private readonly Stack<IDisposable> disposables = new Stack<IDisposable>();
+        
+        public void Dispose()
+        {
+            IDisposable disposable;
+            do
+            {
+                lock (this.disposables)
+                {
+                    disposable = this.disposables.Count > 0
+                        ? this.disposables.Pop()
+                        : null;
+                }
+                disposable?.Dispose();
+            } while (disposable != null);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(IDisposable disposable)
+        {
+            lock (this.disposables)
+            {
+                this.disposables.Push(disposable);
+            }
+        }
+    }
+}
