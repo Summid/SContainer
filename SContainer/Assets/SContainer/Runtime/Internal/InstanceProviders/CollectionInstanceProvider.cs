129996d05b033aa6712b567a0af082e29772ea2a
Summid <summid@foxmail.com>
2024-04-18 17:27:00 +0800

    CollectionInstanceProvider collect from parent scopes

--- SContainer/Assets/SContainer/Runtime/Internal/InstanceProviders/CollectionInstanceProvider.cs
+++ SContainer/Assets/SContainer/Runtime/Internal/InstanceProviders/CollectionInstanceProvider.cs
@@ -53,18 +53,64 @@ namespace SContainer.Runtime.Internal
         
         public object SpawnInstance(IObjectResolver resolver)
         {
-            //TODO: when resolver is scopeObjectResolver
+            if (resolver is IScopedObjectResolver scope)
+            {
+                var entirelyRegistrations = this.CollectFromParentScopes(scope);
+                return this.SpawnInstance(resolver, entirelyRegistrations);
+            }
             return this.SpawnInstance(resolver, this.registrations);
         }
 
         internal object SpawnInstance(IObjectResolver resolver, IReadOnlyList<Registration> registrations)
         {
-            var array = Array.CreateInstance(this.ElementType, this.registrations.Count);
-            for (var i = 0; i < this.registrations.Count; i++)
+            var array = Array.CreateInstance(this.ElementType, registrations.Count);
+            for (var i = 0; i < registrations.Count; i++)
             {
                 array.SetValue(resolver.Resolve(registrations[i]), i);
             }
             return array;
+        }
+
+        internal List<Registration> CollectFromParentScopes(IScopedObjectResolver scope, bool localScopeOnly = false)
+        {
+            if (scope.Parent == null)
+            {
+                return this.registrations;
+            }
+
+            var finderType = this.InterfaceTypes[0];
+            List<Registration> mergedRegistrations = null;
+
+            scope = scope.Parent;
+            while (scope != null)
+            {
+                if (scope.TryGetRegistration(finderType, out var registration) &&
+                    registration.Provider is CollectionInstanceProvider parentCollection)
+                {
+                    if (mergedRegistrations == null)
+                    {
+                        // TODO: object pooling
+                        mergedRegistrations = new List<Registration>(this.registrations);
+                    }
+
+                    if (localScopeOnly)
+                    {
+                        foreach (var x in parentCollection.registrations)
+                        {
+                            if (x.Lifetime != Lifetime.Singleton)
+                            {
+                                mergedRegistrations.Add(x);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        mergedRegistrations.AddRange(parentCollection.registrations);
+                    }
+                }
+                scope = scope.Parent;
+            }
+            return mergedRegistrations ?? this.registrations;
         }
     }
 }
