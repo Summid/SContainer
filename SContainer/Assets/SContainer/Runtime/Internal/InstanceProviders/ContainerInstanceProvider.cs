8b86f8ee389cfbe468f2dd60cddfed2623befe33
Summid <summid@foxmail.com>
2024-04-21 20:37:38 +0800

    add container instance provider

---
+++ SContainer/Assets/SContainer/Runtime/Internal/InstanceProviders/ContainerInstanceProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SContainer.Runtime.Internal
+{
+    /// <summary>
+    /// 字如其名，只返回 Container，即 IObjectResolver 对象；“每个作用域将会自动注册自身”功能会用到
+    /// </summary>
+    internal sealed class ContainerInstanceProvider : IInstanceProvider
+    {
+        public static readonly ContainerInstanceProvider Default = new ContainerInstanceProvider();
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public object SpawnInstance(IObjectResolver resolver) => resolver;
+    }
+}
