0d289405783996c228b1f49f63906b7adb2c88bd
Summid <summid@foxmail.com>
2024-05-04 15:27:52 +0800

    IObjectResolverUnityExtensions: bug fix
    fix for when a game object throws during instantiation

--- SContainer/Assets/SContainer/Runtime/Unity/IObjectResolverUnityExtensions.cs
+++ SContainer/Assets/SContainer/Runtime/Unity/IObjectResolverUnityExtensions.cs
@@ -198,14 +198,18 @@ namespace SContainer.Runtime.Unity
             var wasActive = prefab.activeSelf;
             prefab.SetActive(false);
 
-            var instance = UnityEngine.Object.Instantiate(prefab, parent, worldPositionStays);
-            
-            SetName(instance, prefab);
-
-            resolver.InjectGameObject(instance);
-
-            prefab.SetActive(wasActive);
-            instance.SetActive(wasActive);
+            GameObject instance = null;
+            try
+            {
+                instance = UnityEngine.Object.Instantiate(prefab, parent, worldPositionStays);
+                SetName(instance, prefab);
+                resolver.InjectGameObject(instance);
+            }
+            finally
+            {
+                prefab.SetActive(wasActive);
+                instance?.SetActive(wasActive);
+            }
 
             return instance;
         }
