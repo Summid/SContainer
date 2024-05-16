dd86fdb3aec169c11882a542e2de395ea8a56369
Summid <summid@foxmail.com>
2024-05-16 15:21:25 +0800

    Add FuncRegistrationBuilder and Provider

---
+++ SContainer/Assets/SContainer/Runtime/Internal/FuncRegistrationBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SContainer.Runtime.Internal
+{
+    internal sealed class FuncRegistrationBuilder : RegistrationBuilder
+    {
+        private readonly Func<IObjectResolver, object> implementationProvider;
+
+        public FuncRegistrationBuilder(
+            Func<IObjectResolver, object> implementationProvider,
+            Type implementationType,
+            Lifetime lifetime) : base(implementationType, lifetime)
+        {
+            this.implementationProvider = implementationProvider;
+        }
+
+        public override Registration Build()
+        {
+            var spawner = new FuncInstanceProvider(this.implementationProvider);
+            return new Registration(this.ImplementationType, this.Lifetime, this.InterfaceTypes, spawner);
+        }
+    }
+}
---
+++ SContainer/Assets/SContainer/Runtime/Internal/InstanceProviders/FuncInstanceProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace SContainer.Runtime.Internal
+{
+    internal sealed class FuncInstanceProvider : IInstanceProvider
+    {
+        private readonly Func<IObjectResolver, object> implementationProvider;
+
+        public FuncInstanceProvider(Func<IObjectResolver, object> implementationProvider)
+        {
+            this.implementationProvider = implementationProvider;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public object SpawnInstance(IObjectResolver resolver) => this.implementationProvider(resolver);
+    }
+}
